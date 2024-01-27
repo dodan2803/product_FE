f29ac170afd78d9ac173d7f5d5ad19d68773f280
Dân <138743748+Dan-k21@users.noreply.github.com>
2024-01-28 00:10:29 +0700

    fe

--- product/search.js
+++ product/search.js
@@ -27,3 +27,34 @@ function searchByName() {
             document.getElementById("main").innerHTML = html;
         })
 }
+
+function findByPrice() {
+    let minPrice = document.getElementById("minPrice").value;
+    let maxPrice = document.getElementById("maxPrice").value;
+    axios.get(`http://localhost:8080/products/price?min=${minPrice}&max=${maxPrice}`).then(function (response) {
+            let productList = response.data;
+            let html = ` <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Category</th>
+            <th colspan="2">Action</th>
+        </tr>`
+            for (let i = 0;i < productList.length; i++) {
+                html += `        
+        <tr>
+            <td>${productList[i].id}</td>
+            <td>${productList[i].name}</td>
+            <td>${productList[i].price}</td>
+            <td>${productList[i].quantity}</td>
+            <td>${productList[i].category.name}</td>
+            <td><button onclick="showFormEdit(${productList[i].id})">Sửa</button></td>
+            <td><button onclick="remove(${productList[i].id})">Xóa</button></td>
+        </tr>`
+            }
+            html += '</table>'
+            document.getElementById("main").innerHTML = html;
+        })
+}
